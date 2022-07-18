1c51680d6bd3c493db794af1637b6ecc817664bf
Sope Shen <shenshouer51@gmail.com>
2022-07-18 20:16:02 +0800

    新增客户联系模块结构与readme更新

--- README.md
+++ README.md
@@ -14,3 +14,30 @@
 
 - 将 `.env.sample` 改名为 `.env`, 替换`.env`中的`CORP_ID`与`CORP_SECRET`
 - 修改[examples](./examples)对应rs文件中的参数后，使用`cargo run --example xxx `运行，如运行user示例： `cargo run --example user`
+
+## todo list
+
+- [✓] 获取access_token [实现](./src/client/mod.rs)
+- [ ] 通讯录管理
+    - [✓] 成员管理 [实现](./src/client/contact/user/mod.rs) [示例](./examples/user.rs)
+    - [✓] 部门管理 [实现](./src/client/contact/department/mod.rs) [示例](./examples/department.rs)
+    - [ ] 标签管理
+    - [ ] 异步批量接口
+    - [ ] 互联企业
+    - [ ] 异步导出接口
+- [ ] 客户联系
+- [ ] 微信客户
+- [ ] 身份验证
+- [ ] 应用管理
+- [ ] 消息推送
+- [ ] 素材管理
+- [ ] OA
+- [ ] 效率工具
+- [ ] 企业支付
+- [ ] 企业互联
+- [ ] 上下游
+- [ ] 会话内容存档
+- [ ] 电子发票
+- [ ] 家校沟通
+- [ ] 家校应用
+- [ ] 政民沟通
--- examples/external_contact.rs
+++ examples/external_contact.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use dotenv::dotenv;
 use tracing::info;
-use wecom_rs::{Client, EnterpriseServiceManager, OnJobInheritManager, ParamsTransferResult};
+use wecom_rs::{Client, EnterpriseServiceManager, OnJobInheritManager};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -13,11 +13,11 @@ async fn main() -> Result<()> {
     let user_ids = c.get_follow_user_list().await?;
     info!("user_id: {}", serde_json::to_string(&user_ids)?);
 
-    let params = ParamsTransferResult::default(); // <- 修改参数
-    let transfer_result = c.transfer_result(params).await?;
-    info!(
-        "transfer_result: {}",
-        serde_json::to_string(&transfer_result)?
-    );
+    // let params = wecom_rs::ParamsTransferResult::default(); // <- 修改参数
+    // let transfer_result = c.transfer_result(params).await?;
+    // info!(
+    //     "transfer_result: {}",
+    //     serde_json::to_string(&transfer_result)?
+    // );
     Ok(())
 }
